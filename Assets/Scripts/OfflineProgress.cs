c8c28e87d6e9f987937ab5a92cb043c0f89156f8
Chr1st1anAML <YouKnowMe2777@gmail.com>
2022-12-15 23:30:57 +0100

    Fixed OfflineProgress weil krass
    Looser TBS

--- Assets/Scripts/OfflineProgress.cs
+++ Assets/Scripts/OfflineProgress.cs
@@ -10,24 +10,30 @@ public class OfflineProgress : MonoBehaviour
 
     void Start()
     {
+        StartCoroutine(AddOfflineProgression());
+    }
+    private void OnApplicationQuit()
+    {
+        PlayerPrefs.SetString("LAST_LOGIN", DateTime.Now.ToString());
+    }
+    public IEnumerator AddOfflineProgression()
+    {
+        yield return new WaitForSeconds(2);
         if (PlayerPrefs.HasKey("LAST_LOGIN"))
         {
             DateTime lastLogin = DateTime.Parse(PlayerPrefs.GetString("LAST_LOGIN"));
 
             TimeSpan ts = DateTime.Now - lastLogin;
 
-            tss.Tokens += (int)ts.TotalSeconds * (2 * tss.TokensMultiplyer);
+            tss.Tokens += (int)ts.TotalSeconds * 2 * tss.TokensMultiplyer;
 
-            print((int)ts.TotalSeconds * (2 * tss.TokensMultiplyer));
+            print((int)ts.TotalSeconds * 2 * tss.TokensMultiplyer);
 
             print(ts.TotalSeconds.ToString());
-        } else
+        }
+        else
         {
             print("lol");
         }
-    }
-    private void OnApplicationQuit()
-    {
-        PlayerPrefs.SetString("LAST_LOGIN", DateTime.Now.ToString());
     }
 }
