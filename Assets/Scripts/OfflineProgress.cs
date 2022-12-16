016e4686dc4301e1ce873a3996c30d697fac7eef
Chr1st1anAML <YouKnowMe2777@gmail.com>
2022-12-16 01:14:54 +0100

    Fixed OfflineProgression
    -.-

--- Assets/Scripts/OfflineProgress.cs
+++ Assets/Scripts/OfflineProgress.cs
@@ -2,23 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using UnityEngine.UI;
 
 public class OfflineProgress : MonoBehaviour
 {
     public MinionScript ms;
     public TokensScript tss;
+    public Text offlineearnings;
+    public Text offlineseconds;
+
 
     void Start()
     {
-        StartCoroutine(AddOfflineProgression());
+    }
+    public void OnApplicationFocus(bool focus)
+    {
+        if (focus)
+        {
+            StartCoroutine(AddOfflineProgression());
+            StartCoroutine(AddOfflineTime());
+        } else
+        {
+            PlayerPrefs.SetString("LAST_LOGIN", DateTime.Now.ToString());
+        }
     }
     private void OnApplicationQuit()
     {
         PlayerPrefs.SetString("LAST_LOGIN", DateTime.Now.ToString());
     }
+    public IEnumerator AddOfflineTime()
+    {
+        yield return new WaitForSeconds(1);
+        DateTime lastLogin = DateTime.Parse(PlayerPrefs.GetString("LAST_LOGIN"));
+        TimeSpan ts = DateTime.Now - lastLogin;
+        offlineseconds.text = "Off-Time: Sec: " + (int)ts.TotalSeconds;
+        StartCoroutine(AddOfflineProgression());
+        yield return new WaitForSeconds(8);
+        offlineseconds.text = "";
+    }
     public IEnumerator AddOfflineProgression()
     {
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(1);
         if (PlayerPrefs.HasKey("LAST_LOGIN"))
         {
             DateTime lastLogin = DateTime.Parse(PlayerPrefs.GetString("LAST_LOGIN"));
@@ -30,10 +54,13 @@ public class OfflineProgress : MonoBehaviour
             print((int)ts.TotalSeconds * 2 * tss.TokensMultiplyer);
 
             print(ts.TotalSeconds.ToString());
+            offlineearnings.text = "Off-Earnings: "+ (int)ts.TotalSeconds * 2 * tss.TokensMultiplyer;
+            yield return new WaitForSeconds(8);
+            offlineearnings.text = "";
         }
         else
         {
-            print("lol");
+            print("ERROR -> OfflineProgressionen Failure.");
         }
     }
 }
--- Assets/Scripts/ShopSystem.cs
+++ Assets/Scripts/ShopSystem.cs
@@ -9,21 +9,31 @@ public class ShopSystem : MonoBehaviour
     public GameObject ShopImage;
     public GameObject ShopText;
     public GameObject HutPriceText;
+    public GameObject RolexPriceText;
     public Animator tween;
 
     public TokensScript ts;
 
     public GameObject Hut;
+    public GameObject Rolex;
 
-    public float HutPrice = 25;
+    public float HutPrice = 150;
+    public float RolexPrice = 1500;
     void Start()
     {
-        HutPrice = PlayerPrefs.GetFloat("HutPrice", 25);
+        HutPrice = PlayerPrefs.GetFloat("HutPrice", 150);
         HutPriceText.GetComponent<Text>().text = HutPrice.ToString() + "$";
+        RolexPrice = PlayerPrefs.GetFloat("RolexPrice", 1500);
+        RolexPriceText.GetComponent<Text>().text = RolexPrice.ToString() + "$";
         int hutisactive = PlayerPrefs.GetInt("HutActive", 0);
+        int rolexisactive = PlayerPrefs.GetInt("RolexActive", 0);
         if (hutisactive == 1)
         {
             Hut.active = true;
+        }
+        if (rolexisactive == 1)
+        {
+            Rolex.active = true;
         }
     }
     public void OnShopClick()
@@ -50,10 +60,34 @@ public class ShopSystem : MonoBehaviour
         PlayerPrefs.SetFloat("Tokens", ts.Tokens);
         ts.TokensMultiplyer = ts.TokensMultiplyer + 0.2f;
         PlayerPrefs.SetFloat("TokensMultiplyer", ts.TokensMultiplyer);
-        HutPrice = HutPrice * 1.6f;
+        HutPrice = HutPrice * 2f;
         HutPrice = Mathf.Round(HutPrice * 100.0f) * 0.01f;
         HutPriceText.GetComponent<Text>().text = HutPrice.ToString() + "$";
         PlayerPrefs.SetFloat("HutPrice", HutPrice);
+        float roundedtokensfloat = Mathf.Round(ts.Tokens * 100.0f) * 0.01f;
+        float roundedmultiplyerfloat = Mathf.Round(ts.TokensMultiplyer * 100.0f) * 0.01f;
+        ts.TokensText.text = roundedtokensfloat + "$";
+        ts.MultiplierText.text = roundedmultiplyerfloat + "x";
+    }
+    public void OnPurchaseRolex()
+    {
+        if (ts.Tokens < RolexPrice)
+        {
+            return;
+        }
+        if (Rolex.active == false)
+        {
+            Rolex.active = true;
+        }
+        PlayerPrefs.SetInt("RolexActive", 1);
+        ts.Tokens = ts.Tokens - RolexPrice;
+        PlayerPrefs.SetFloat("Tokens", ts.Tokens);
+        ts.TokensMultiplyer = ts.TokensMultiplyer + 0.6f;
+        PlayerPrefs.SetFloat("TokensMultiplyer", ts.TokensMultiplyer);
+        RolexPrice = RolexPrice * 3f;
+        RolexPrice = Mathf.Round(RolexPrice * 100.0f) * 0.01f;
+        RolexPriceText.GetComponent<Text>().text = RolexPrice.ToString() + "$";
+        PlayerPrefs.SetFloat("RolexPrice", RolexPrice);
         float roundedtokensfloat = Mathf.Round(ts.Tokens * 100.0f) * 0.01f;
         float roundedmultiplyerfloat = Mathf.Round(ts.TokensMultiplyer * 100.0f) * 0.01f;
         ts.TokensText.text = roundedtokensfloat + "$";
